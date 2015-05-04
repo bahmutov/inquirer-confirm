51ecfb51989944bc5d544441d5ce36393c794646
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-05-03 23:14:33 -0400

    unit testing using bdd-stdin

--- test/spec.js
+++ test/spec.js
@@ -1,9 +1,36 @@
+require = require('really-need');
 require('lazy-ass');
 var check = require('check-more-types');
+var bddStdin = require('bdd-stdin');
 var confirm = require('..');
 
 describe('inquirer-confirm', function () {
   it('is is a function', function () {
     la(check.fn(confirm));
   });
+
+  it('returns a promise, resolved on yes', function () {
+    bddStdin('yes\n');
+
+    return confirm('is this ok?')
+      .catch(function () {
+        la(false, 'invalid response, should be resolved on yes');
+      });
+  });
+
+  it('rejected on no', function () {
+    bddStdin('no\n');
+
+    var rejected;
+    return confirm('is this ok?')
+      .then(function () {
+        la(false, 'invalid response, should be rejected on no');
+      }, function () {
+        rejected = true;
+      })
+      .then(function () {
+        la(rejected, 'response was never rejected');
+      });
+  });
+
 });
