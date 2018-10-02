d65b0b4c6b4b8f76c29a51a21b0906d8426818a4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-10-02 14:24:16 -0400

    fix: remove bluebird

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 var ask = require('inquirer');
-var Promise = require('bluebird');
 
 module.exports = function confirm(question, extra) {
   question = question || 'Are you sure?';
 
-  return new Promise(function (resolve, reject) {
-
-    ask.prompt({
-      name: 'confirm',
-      type: 'confirm',
-      message: question
-    }, function (answers) {
-      if (answers.confirm) {
-        return resolve(extra);
-      } else {
-        return reject(extra);
-      }
-    });
-
+  ask.prompt({
+    name: 'confirm',
+    type: 'confirm',
+    message: question
+  }, function (answers) {
+    if (answers.confirm) {
+      return Promise.resolve(extra);
+    } else {
+      return Promise.reject(extra);
+    }
   });
 };
